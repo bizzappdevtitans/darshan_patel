b9f6d35db5b3dc3315e13600ad19bb65d415e974
BAD <BAD@bizzappdev.com>
2023-10-11 12:27:23 +0530

    [WIP][T00457]set-delivery-address

--- set_delivery_address/models/sale_order.py
+++ set_delivery_address/models/sale_order.py
@@ -1,14 +1,29 @@
-from odoo import fields, models, api
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    partner_id = fields.Many2one(comodel_name="res.partner")
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        super(SaleOrder, self).onchange_partner_id()
+        list_partner = []
+        for record in self.partner_id:
+            for child in record.child_ids:
+                if child.address_verified is True:
+                    list_partner.append(child.name)
 
-    @api.depends('partner_id')
-    def onchange_user_id(self):
-
-        print("onchange_partner_id method called")
-
-        return super(SaleOrder, self).onchange_user_id()
+        for record in self.partner_id:
+            for child in record.child_ids:
+                if len(list_partner) > 1:
+                    if child.name == list_partner[0]:
+                        values = {
+                            'partner_shipping_id': child.id,
+                        }
+                        self.update(values)
+                else:
+                    if child.address_verified is True:
+                        values = {
+                            'partner_shipping_id': child.id,
+                        }
+                        self.update(values)
