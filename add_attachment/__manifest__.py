f7bdea8566d9de7b50d0480b000a7baba2a864c8
BAD <BAD@bizzappdev.com>
2023-10-11 18:58:47 +0530

    [WIP][T00463]add-attachment: add attachment on sale order

---
+++ add_attachment/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "add_attachment",
+    "version": "15.0.0.0.1",
+    "category": "add_attachment",
+    "summary": "add attachment on sale order",
+    "author": "Bizzappdev",
+    "website": "http://www.Bizzappdev.com",
+    "description": """
+     Bizzappdev
+    """,
+    "depends": ["sale_management"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "license": "LGPL-3",
+}
---
+++ add_attachment/models/sale_order.py
@@ -0,0 +1,5 @@
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
