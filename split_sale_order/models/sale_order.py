a5d5be831d25a1b6a38ee4d86f6c58f9c9d065c9
BAD <BAD@bizzappdev.com>
2023-12-11 17:40:34 +0530

    [IMP][T6982]Split-SO:Added product in main sale order

--- split_sale_order/models/sale_order.py
+++ split_sale_order/models/sale_order.py
@@ -6,3 +6,10 @@ class SaleOrder(models.Model):
 
     # field for set main sale order number #T6982
     sale_order_reference = fields.Char(readonly=True)
+
+    def copy(self, default=None):
+        """Added copy method for set default single space on copied splited sale
+        order #T6982"""
+        default = dict(default or {})
+        default["sale_order_reference"] = " "
+        return super(SaleOrder, self).copy(default=default)
